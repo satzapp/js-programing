3b4a47439473037136f140d274e932158d740018
Satz K <satzapplications@gmail.com>
2021-12-30 04:42:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// reverse string
+var name = 'Sathishkumar Manoharan';
+
+function reverseString(str, separator) {
+  return str.split(separator).reverse().join(separator);
+}
+
+var reverseStr = reverseString(name, "");
+
+console.log(reverseStr);
+
+
+// find a big number
+var arr = [1,2,3,4,5,6,7,8,9];
+
+console.log(Math.max(...arr))
+
+var jsonObj = [{
+      "name": "B",
+      "description": "South Georgia and the South Sandwich",
+      "status": "ok",
+      "location": {
+        "created_at": "2021-12-08T05:36:51.951308",
+        "address": "Boston, MA 02128, USA",
+        "phone": "8744024236",
+        "name": "Boston11",
+      }
+    },
+    {
+      "name": "A",
+      "description": "South Georgia and the South Sandwich",
+      "status": "ok",
+      "location": {
+        "created_at": "2021-12-08T05:36:51.951308",
+        "address": "Boston, MA 02128, USA",
+        "phone": "8744024236",
+        "name": "India",
+      }
+    },
+    {
+      "name": "C",
+      "description": "South Georgia and the South Sandwich",
+      "status": "ok",
+      "location": {
+        "created_at": "2021-12-08T05:36:51.951308",
+        "address": "Boston, MA 02128, USA",
+        "phone": "8744024236",
+        "name": "Austrlia",
+      }
+    }];
+
+console.log(
+  jsonObj.sort((x,y)=> x.location.name.localeCompare(y.location.name))
+  )
+
+function sortArrays(arr) {
+    // var sortedlocationData = [];
+    // for(let data in arr) {
+    //     sortedlocationData.push(arr[data]['location']);
+    // }
+
+    // return sortedlocationData.sort(sortedlocationData.name);
+  
+    return arr.sort(function(a,b){return obj[b]-obj[a]})
+} 
