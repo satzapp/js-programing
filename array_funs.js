aacbe601cf98aaa7955790504f2d4289fe2c12d3
Satz K <satzapplications@gmail.com>
2021-12-30 06:06:29 +0000

    array sort functions

--- array_funs.js
+++ array_funs.js
@@ -1,1 +1,29 @@
-console.log('hi')
+var arryObj = [ 'Sathishkumar', 'Praveenkumar', 'Vishnukumar', 'Ram', 'Prem'];
+
+var items = [
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic', value: 13 },
+  { name: 'Zeros', value: 37 }
+];
+
+// sort array
+
+let sortedObj = arryObj.sort();
+
+console.log(sortedObj);
+
+// sort by associative array name
+let itemsSort = items.sort((a,b) => {
+  return a.name.localeCompare(b.name);
+});
+
+// sort by associative array number
+let itemSortByNumber = items.sort((x,y)=>{
+  return x.value - y.value;
+})
+
+
+console.log(itemSortByNumber)
